842dc062d5f6a175d9031d229d2e6583fcd0e1ba
Aaron Choi <aaronschoilcw@gmail.com>
2022-02-08 17:19:11 -0800

    navbar

---
+++ components/navbar.tsx
@@ -0,0 +1,19 @@
+import Link from "next/link";
+
+const Navbar = () => {
+    <nav>
+    <Link href="/">
+        <a>HOME</a>
+    </Link>
+    <Link>
+        <a>SERVICES</a>
+    </Link>
+    <Link>
+        <a>GALLERY</a>
+    </Link>
+    <Link>
+    <a>CLIENTS</a></Link>
+    <Link><a>STORY</a></Link>
+    <Link><a>CONTACT</a></Link>
+    </nav>
+}
