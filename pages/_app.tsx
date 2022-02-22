81e21a10c4d280cf125ae06a8e9c1fd1f40c895d
Aaron Choi <connect@aaronschoi.com>
2022-02-21 22:13:46 -0800

    added _document.tsx

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -11,14 +11,6 @@ import 'bootstrap/dist/css/bootstrap.css'
 const MyApp = ({ Component, pageProps }) => {
     return (
         <>
-            <Head>
-                <meta charSet="UTF-8" />
-                <meta http-equiv="X-UA-Compatible" content="IE=edge" />
-                <meta
-                    name="viewport"
-                    content="width=device-width, initial-scale=1.0"
-                />
-            </Head>
             <ThemeProvider theme={theme}>
                 {/* // easily implement dark mode here */}
                 <GlobalStyle />
