58e524f24f3852f69c186e9cfa05bb03df2adfb8
Aaron Choi <connect@aaronschoi.com>
2022-03-08 17:16:26 -0800

    Service Landing resize

--- components/Services/ServicesHome.tsx
+++ components/Services/ServicesHome.tsx
@@ -1,8 +1,8 @@
 import styled from 'styled-components'
-import MinFullPage from '../GeneralPurposeComponents/MinFullPage'
 import ServicesNavBar from './ServicesNavBar/ServicesNavBar'
 
-const PinToBottomNavBar = styled(MinFullPage)`
+const PinToBottomNavBar = styled.div`
+    min-height: 100vh;
     display: flex;
     flex-direction: column;
     justify-content: space-between;
