fe94ded53c9a273c0fcfc56194250b9642bcecfd
Ben Kang <ben-mini@bens-mac-mini.lan>
2022-03-30 11:55:24 -0700

    add hyperlink to logo

--- components/MainLayout/Header/Logo/Logo.tsx
+++ components/MainLayout/Header/Logo/Logo.tsx
@@ -1,7 +1,15 @@
 import Image from 'next/image'
+import Link from 'next/link'
+import styled from 'styled-components'
+
+const ImageContainer = styled.div`
+    cursor: pointer;
+`
 
 const Logo = () => {
-    return <Image src="/logo.png" width="146" height="95" alt="Raise Your Spirits" />
+    return (
+        <Link href="/" passHref><ImageContainer><Image src="/logo.png" width="146" height="95" alt="Raise Your Spirits" /></ImageContainer></Link>
+    )
 }
 
 export default Logo
